e0314e6e23fa6ffa83db99f075d69243ff2c616a
TilenOkretic <tilen.okretic@gmail.com>
2022-09-25 18:32:31 +0200

    :sparkles: createElement generic implementation :tada:

--- src/create.ts
+++ src/create.ts
@@ -4,7 +4,7 @@
  * @param {String} id DOM id
  * @returns newly created DOM element
  */
-export const createElement = (type: string, id: string): Element => {
+export const createElement = <K extends keyof HTMLElementTagNameMap>(type: K, id: string): HTMLElementTagNameMap[K] => {
   const e = document.createElement(type);
   e.id = id;
   return e;
